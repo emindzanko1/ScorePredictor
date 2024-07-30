38d988e0c96edf802b9a6f65614c350bf02fd671
emin.dzanko <emin.dzanko@isatis.com>
2024-07-30 16:23:30 +0200

    Implemented backend register and login

---
+++ API/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
+    public int Points { get; set; }
+    public string? Picture { get; set; }
+    public required string FullName { get; set; }
+}
